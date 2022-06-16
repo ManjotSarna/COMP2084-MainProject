8db71e8731fc7563c460e24564d3266883a445a1
Manjot_Sarna <manjotsarna44@gmail.com>
2022-06-15 21:52:31 -0400

    Initial Project Commit

---
+++ COMP2084-MAINPROJECT/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace COMP2084_MAINPROJECT.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
